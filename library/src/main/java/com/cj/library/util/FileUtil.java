0d4f3ec69d2bb95ad1dd9428df9f8052cdf1caf8
zhangchaojun <565978459@qq.com>
2023-01-29 10:22:30 +0800

    升级版本

--- library/src/main/java/com/cj/library/util/FileUtil.java
+++ library/src/main/java/com/cj/library/util/FileUtil.java
@@ -1,6 +1,15 @@
 package com.cj.library.util;
 
+import java.io.File;
+
 public class FileUtil {
 
 
+    public static boolean isFile(File file) {
+        if (file == null) {
+            return false;
+        }
+
+        return file.isFile();
+    }
 }
